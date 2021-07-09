0ead6d566917f84f370c07f24536d4839f426be3
johanqnms <johanqnms@gmail.com>
2021-07-09 08:13:52 -0500

    Punto en el webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    entry: '/src/App.js',
+    entry: './src/App.js',
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: 'index_bundle.js',
